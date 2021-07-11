f6944b8e1611fe0061e1365939145db744d4b1c1
Voytsh <wojak08@icloud.com>
2021-07-11 15:44:53 +0200

    Initial Commit

---
+++ Ping/PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PingApp.swift
+//  Ping
+//
+//  Created by Filip Wojcieszak on 11/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
